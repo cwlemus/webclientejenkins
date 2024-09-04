30a748912e5ba69061df6f97add8edc99515f0fb
Carlos Lemus <cwlemus@gmail.com>
2024-09-03 22:08:02 -0600

    inicio

---
+++ webcliente/src/main/java/com/home/webcliente/ClientePersona.java
@@ -0,0 +1,23 @@
+package com.home.webcliente;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.home.dtos.PersonaDto;
+
+@RestController
+public class ClientePersona {
+	@RequestMapping("/personascliente")
+	public List<PersonaDto> listPersona(){		
+		RestTemplate plantilla = new RestTemplate();
+		ResponseEntity<PersonaDto[]> lista = plantilla.getForEntity("http://localhost:8099/personas", PersonaDto[].class);
+		PersonaDto[] nuevaLista = lista.getBody();
+		return Arrays.asList(nuevaLista);
+	}
+
+}
